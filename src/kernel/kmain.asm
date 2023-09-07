ecdbaffa4a92a53ba16853ef9c22abbf385714f9
Wowkster <49880655+wowkster@users.noreply.github.com>
2023-09-07 12:16:06 -0400

    refactor: :recycle: rename video.asm to vga.asm

--- src/kernel/kmain.asm
+++ src/kernel/kmain.asm
@@ -2,21 +2,22 @@ org 0x10000
 bits 32
 
 kmain:
-    call clear_screen
+    .clear_vga_buffer:
+        call clear_screen
 
-.print_hello:
-    mov esi, kernel_msg_1
-    call kprintln
+    .print_hello:
+        mov esi, kernel_msg_1
+        call kprintln
 
-    mov esi, kernel_msg_2
-    call kprintln
-
+        mov esi, kernel_msg_2
+        call kprintln
+    
 halt:
     ; Halt the processor
     hlt
     jmp halt
 
-%include "video.asm"
+%include "vga.asm"
 
 kernel_msg_1: db 'Hello from the kernel!', 0
 kernel_msg_2: db 'This tetx is on another line!', 0
