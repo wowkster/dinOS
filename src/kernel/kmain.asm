7fc6eea872439b1fd8baa512d2eef6382f9289e5
Wowkster <49880655+wowkster@users.noreply.github.com>
2023-09-09 19:27:04 -0400

    feat: :sparkles: add interrupt init message

--- src/kernel/kmain.asm
+++ src/kernel/kmain.asm
@@ -5,15 +5,16 @@ kmain:
     .clear_vga_buffer:
         call clear_screen
 
-    .print_hello:
-        mov esi, kernel_msg_1
-        call kprintln
-
-        mov esi, kernel_msg_2
+    .print_loaded:
+        mov esi, kernel_loaded_msg
         call kprintln
     
     .enable_interrupts:
         call init_interrupts
+    
+    .interrupts_initialized:
+        mov esi, interrupts_initialized_msg
+        call kprintln
 
 halt:
     ; Halt the processor
@@ -23,5 +24,5 @@ halt:
 %include "vga.asm"
 %include "interrupt/init.asm"
 
-kernel_msg_1: db 'Hello from the kernel!', 0
-kernel_msg_2: db 'This tetx is on another line!', 0
+kernel_loaded_msg: db 'Kernel kmain called!', 0
+interrupts_initialized_msg: db 'Interrupts initialized!', 0
