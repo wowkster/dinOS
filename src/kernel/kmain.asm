9239a8d461d0b2d961baa9a2f80a783d9a5be8e5
Adrian Wowk <dev@adrianwowk.com>
2025-06-28 13:22:25 -0500

    feat: impl shell prompt

--- src/kernel/kmain.asm
+++ src/kernel/kmain.asm
@@ -35,38 +35,103 @@ kmain:
     call vga_enable_cursor
     mkprintln()
 
+    call print_shell_prompt
+
+    ; Create room on the stack for the key event
+
+    push ebp
+    mov ebp, esp
+    sub esp, 4
+
     ; Consume key events
     .keyboard_loop:
+        ; Poll the keyboard driver
         call kb_get_next_key_event
 
-        mkprint('Got key event: ')
-        call kprint_dword
+        ; Store the key event on the stack
+        mov [ebp - 4], eax
 
-        ; ===========================
+        ; Skip event unless its a key down event
+        cmp ah, 1
+        jne .keyboard_loop
 
-        mkprint(' has shift = ')
-        call kb_key_event_has_shift
-        call kprint_zf_as_bool
+        ; Check the keycode to see what kind of operation we need to do
+        mov al, [ebp - 1]
 
-        ; ===========================
+        cmp al, KB_KC_ENTER
+        je .handle_enter
 
-        mkprint(' has ctrl = ')
-        call kb_key_event_has_ctrl
-        call kprint_zf_as_bool
+        cmp al, KB_KC_BACKSPACE
+        je .handle_backspace
 
-        ; ===========================
+        ; If the prompt buffer is already full then we ignore this character
+        xor ebx, ebx
+        mov bl, [_prompt_buffer_idx]
+        cmp bl, PROMPT_BUFFER_MAX_LEN
+        jge .keyboard_loop
 
-        mkprint(' has alt = ')
-        call kb_key_event_has_alt
-        call kprint_zf_as_bool
-        mkprintln()
+        ; If the character is not printable, don't print it
+        mov al, [ebp - 2]
+        cmp al, 0
+        je .keyboard_loop
+
+        ; Print the character
+        call kprint_char
+
+        ; Push to the prompt buffer
+        mov [_prompt_buffer + ebx], al
+        inc bl
+        mov [_prompt_buffer_idx], bl
 
         jmp .keyboard_loop
+
+    .handle_enter:
+        ; Parse and execute the command
+        mkprintln()
+
+        ; Reset the prompt buffer length
+        mov bl, 0
+        mov [_prompt_buffer_idx], bl
+
+        ; Print the new prompt
+        call print_shell_prompt
+
+        jmp .keyboard_loop
+
+    .handle_backspace:
+        ; If the prompt buffer is already empty then we ignore this key event
+        xor ebx, ebx
+        mov bl, [_prompt_buffer_idx]
+        cmp bl, 0
+        je .keyboard_loop
+
+        ; Decrement the prompt buffer length
+        dec bl
+        mov [_prompt_buffer_idx], bl
+
+        call kprint_backspace
+        jmp .keyboard_loop
+
+print_shell_prompt:
+    pushad
+
+    mkprint_color("root@dinos", VGA_COLOR_FG_BRIGHT_GREEN)
+    mkprint(":")
+    mkprint_color("~", VGA_COLOR_FG_BRIGHT_BLUE)
+    mkprint("$ ")
+
+    popad
+    ret
 
 halt:
     ; Halt the processor
     hlt
     jmp halt
+
+_prompt_buffer: times 255 db 0
+_prompt_buffer_idx: db 0
+
+PROMPT_BUFFER_MAX_LEN equ 64
 
 %include "vga.asm"
 %include "interrupt/init.asm"
